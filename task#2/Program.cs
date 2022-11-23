45edd06a1774ff71d98bd77339f461054fedc961
Mikhail Starostin <starostin.mihail@mail.ru>
2022-11-23 22:38:29 +0300

    add task#2

---
+++ task#2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа, которая выдает название дня недели по его номеру
+Console.Clear();
+Console.WriteLine("Введите введите номер дня недели ");
+int num = int.Parse(Console.ReadLine());
+if (num == 1) Console.WriteLine("Этот день - понедельник");
+if (num == 2) Console.WriteLine("Этот день - вторник");
+if (num == 3) Console.WriteLine("Этот день - среда");
+if (num == 4) Console.WriteLine("Этот день - четверг");
+if (num == 5) Console.WriteLine("Этот день - пятница");
+if (num == 6) Console.WriteLine("Этот день - суббота");
+if (num == 7) Console.WriteLine("Этот день - воскресение");
+else {
+    Console.WriteLine("Введенное число не соответствует ни одному из дней недели");
+}
+
+
