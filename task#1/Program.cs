8d60b00010beb60e652e0412754c9b3c8ffb6a59
Mikhail Starostin <starostin.mihail@mail.ru>
2022-11-23 23:17:42 +0300

    fix task#1

--- task#1/Program.cs
+++ task#1/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число для проверки");
+int sqr_num = int.Parse(Console.ReadLine());
+if (Math.Pow(num, 2)==sqr_num)
+{
+ Console.Write($"Число {sqr_num} является квадратом числа {num}");
+}
+else {
+ Console.Write($"Число {sqr_num} не является квадратом числа {num}");
+}
