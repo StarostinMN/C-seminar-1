dddff1d53c2ffc0dfb2ecc242089146e8cf1cb50
Mikhail Starostin <starostin.mihail@mail.ru>
2022-11-23 19:52:44 +0300

    add squaring option 1

---
+++ squaring/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выводит его квадрат
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+int sqr = num * num;
+
+Console.Write("Кdадрат числа " + num + " = " + sqr);
+
+
+
+
