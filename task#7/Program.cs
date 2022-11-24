8368b9fd1c75919406e99d99c202b6d18022e3f5
Mikhail Starostin <starostin.mihail@mail.ru>
2022-11-24 13:24:58 +0300

    add task#7 my option

---
+++ task#7/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+	456 -> 6
+	782 -> 2
+	918 -> 8 */
+Console.Clear();
+Console.Write("Введите трехзначное число ");
+double N = Math.Abs(int.Parse(Console.ReadLine()));
+double last_number = N % 10;
+Console.Write("Последняя цифра введенного вами числа ");
+Console.Write(last_number);
