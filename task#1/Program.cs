fc414d08331cb065d2987d9c8c283d3fe0f92c4b
Mikhail Starostin <starostin.mihail@mail.ru>
2022-11-24 12:12:28 +0300

    add option 2 to task#3 (switch case)

--- task#1/Program.cs
+++ task#1/Program.cs
@@ -4,10 +4,11 @@ Console.WriteLine("Введите первое число");
 int sqr_num = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число для проверки");
 int num = int.Parse(Console.ReadLine());
-if (Math.Pow(num, 2)==sqr_num)
+if (Math.Pow(num, 2) == sqr_num)
 {
- Console.Write($"Число {sqr_num} является квадратом числа {num}");
+    Console.Write($"Число {sqr_num} является квадратом числа {num}");
 }
-else {
- Console.Write($"Число {sqr_num} не является квадратом числа {num}");
+else
+{
+    Console.Write($"Число {sqr_num} не является квадратом числа {num}");
 }
--- task#3/Program.cs
+++ task#3/Program.cs
@@ -2,7 +2,7 @@
 Console.Clear();
 Console.WriteLine("Введите введите номер дня недели ");
 int num = int.Parse(Console.ReadLine());
-if (num == 1) Console.WriteLine("Этот день - понедельник");
+/* if (num == 1) Console.WriteLine("Этот день - понедельник");
 if (num == 2) Console.WriteLine("Этот день - вторник");
 if (num == 3) Console.WriteLine("Этот день - среда");
 if (num == 4) Console.WriteLine("Этот день - четверг");
@@ -10,4 +10,31 @@ if (num == 5) Console.WriteLine("Этот день - пятница");
 if (num == 6) Console.WriteLine("Этот день - суббота");
 if (num == 7) Console.WriteLine("Этот день - воскресение");
 if (num < 1 || num > 7) Console.WriteLine("Введенное число не соответствует ни одному из дней недели");
-
+ */
+switch (num)
+{
+    case 1:
+        Console.WriteLine("Этот день - понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Этот день - вторник");
+        break;
+    case 3:
+        Console.WriteLine("Этот день - среда");
+        break;
+    case 4:
+        Console.WriteLine("Этот день - четверг");
+        break;
+    case 5:
+        Console.WriteLine("Этот день - пятница");
+        break;
+    case 6:
+        Console.WriteLine("Этот день - суббота");
+        break;
+    case 7:
+        Console.WriteLine("Этот день - воскресение");
+        break;
+    default:
+        Console.WriteLine("Введенное число не соответствует ни одному из дней недели");
+        break;
+}
