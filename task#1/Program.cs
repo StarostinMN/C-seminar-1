40eab73002a391c5155fc70ee88f6b52bc4cd92a
Mikhail Starostin <starostin.mihail@mail.ru>
2022-11-24 10:10:48 +0300

    fix task#1

--- task#1/Program.cs
+++ task#1/Program.cs
@@ -1,8 +1,9 @@
-﻿Console.Clear();
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.Clear();
 Console.WriteLine("Введите первое число");
-int num = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число для проверки");
 int sqr_num = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число для проверки");
+int num = int.Parse(Console.ReadLine());
 if (Math.Pow(num, 2)==sqr_num)
 {
  Console.Write($"Число {sqr_num} является квадратом числа {num}");
